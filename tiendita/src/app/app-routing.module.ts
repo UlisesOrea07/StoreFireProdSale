8d5ee71359c9211fd83f68f3796b1dc7eeb180c4
Ulises Orea <j.ulisesorea@gmail.com>
2020-07-11 11:36:31 -0500

    fixed,  jala al cien

--- tiendita/src/app/app-routing.module.ts
+++ tiendita/src/app/app-routing.module.ts
@@ -3,24 +3,39 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-
-    path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () =>
+      import("./pages/tabs/tabs.module").then((m) => m.TabsPageModule),
   },
-  { path: '', loadChildren: () => import('./pages/tab2/tab2.module').then(m => m.Tab2PageModule) },
   {
-    path: 'page-productos',
-    loadChildren: () => import('./pages/page-productos/page-productos.module').then( m => m.PageProductosPageModule)
+    path: "",
+    loadChildren: () =>
+      import("./pages/tab1/tab1.module").then((m) => m.Tab1PageModule),
   },
-   {
+  {
+    path: "",
+    loadChildren: () =>
+      import("./pages/tab2/tab2.module").then((m) => m.Tab2PageModule),
+  },
+  {
+    path: "",
+    loadChildren: () =>
+      import("./pages/tab3/tab3.module").then((m) => m.Tab3PageModule),
+  },
+  {
+    path: "page-productos",
+    loadChildren: () =>
+      import("./pages/page-productos/page-productos.module").then(
+        (m) => m.PageProductosPageModule
+      ),
+  },
+  {
     path: "producto-agregar",
     loadChildren: () =>
       import("./pages/Productos/producto-agregar/producto-agregar.module").then(
         (m) => m.ProductoAgregarPageModule
       ),
   },
-
-
 ];
 @NgModule({
   imports: [
@@ -29,4 +44,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-
--- tiendita/src/app/components/components.module.ts
+++ tiendita/src/app/components/components.module.ts
@@ -1,39 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { ModalComponent } from './modal/modal.component';
-import { HeaderComponent } from './header/header.component';
-
-
-import {ShowCategoriaComponent} from './show-categoria/show-categoria.component'
-import {ShowProductoComponent} from './show-producto/show-producto.component'
-
+import { ModalComponent } from "./modal/modal.component";
+import { HeaderComponent } from "./header/header.component";
 import { BuscadorComponent } from "./buscador/buscador.component";
 import { ImagenpickerComponent } from "./imagenpicker/imagenpicker.component";
-
+import { ShowCategoriaComponent } from "./show-categoria/show-categoria.component";
+import { ShowProductoComponent } from "./show-producto/show-producto.component";
 
 @NgModule({
   declarations: [
-
     ModalComponent,
     HeaderComponent,
-    BuscadorComponent, 
-    ImagenpickerComponent
-  ],
-  imports: [
-    CommonModule,
-    IonicModule
-  ],
-  exports: [
-    ModalComponent,
-    HeaderComponent
     ShowCategoriaComponent,
     ShowProductoComponent,
     BuscadorComponent,
-    ImagenpickerComponent
+    ImagenpickerComponent,
+  ],
+  imports: [CommonModule, IonicModule],
+  exports: [
+    ModalComponent,
+    HeaderComponent,
+    ShowCategoriaComponent,
+    ShowProductoComponent,
+    BuscadorComponent,
+    ImagenpickerComponent,
   ],
 })
-export class ComponentsModule { }
-
+export class ComponentsModule {}
